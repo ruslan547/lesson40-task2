5b64119485d6d04ca71baa42a0e7eb440c9277af
ruslannovik <novik.rv93@gmail.com>
2021-04-24 11:59:32 +0300

    feat: add Empoyee

---
+++ src/main/java/by/itClass/model/beans/Employee.java
@@ -0,0 +1,58 @@
+package by.itClass.model.beans;
+
+public class Employee {
+    private int id;
+    private String name;
+    private String position;
+    private int salary;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String name, String position, int salary) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id +
+                ", name=" + name +
+                ", position=" + position +
+                ", salary=" + salary;
+    }
+}
